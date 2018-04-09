63a6465daea0c0b456d691d5113e715f342eb66b
Philippe Trudel <p.trudel1996@gmail.com>
2018-04-09 18:44:13 -0400

    react setup part finished

---
+++ react-redux-boiler-plate/src/constants/constants.js
@@ -0,0 +1,6 @@
+/**
+ * Function : Keeping all your constants of your apps such as (but not limited to) :
+ *          - api's url, 
+ *          - actions' type, 
+ *          - etc.
+ */ 
---
+++ react-redux-boiler-plate/src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+        </div>
+      );
+    }
+  }
+
+ReactDOM.render(
+    <App />
+, document.getElementById('root'));
